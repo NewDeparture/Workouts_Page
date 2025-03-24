d98f8f33c35d4165f685e05806d8e9a7b81e1386
Departure <29082270+NewDeparture@users.noreply.github.com>
2025-03-24 22:26:14 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,15 +11,15 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteTitle: 'LYX\' Workouts Map',
+  siteUrl: 'https://newdeparture.github.io/Workouts_Page/',
+  logo: 'https://img.picgo.net/2025/03/17/Anime026e152421d44d6a.md.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'https://github.com/ben-29/workouts_page',
     },
     {
       name: 'About',
