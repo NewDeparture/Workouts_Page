c749b0ef15aa3ad65bb1d84709cdea8ba3c237da
Panda <29082270+NewDeparture@users.noreply.github.com>
2025-09-23 15:33:58 +0800

    update personal info

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,9 +16,9 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteTitle: 'LYX\'S Workouts Map',
+  siteUrl: 'https://newdeparture.github.io/Workouts_Page/',
+  logo: 'https://img.picgo.net/2025/03/17/Anime026e152421d44d6a.md.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
